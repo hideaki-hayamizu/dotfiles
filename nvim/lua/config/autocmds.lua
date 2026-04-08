da4369ddc6f7ea0c1a96b80b57a009e6f7622c4e
hideaki-hayamizu <hikaedi18hei06@gmail.com>
2026-04-08 17:12:03 +0900

    NVIM: add a autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd('TermOpen',{
     vim.cmd.startinsert()
   end
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
+    if client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+  end
+})
